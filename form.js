b26f2a87ea132db5f45646ea959a469a875377a0
musicbender <musicbender@gmail.com>
2015-09-28 20:23:49 -0700

    Three Calculators
    Now with brew, moisture loss, and roast development calculators.

--- form.js
+++ form.js
@@ -1,5 +1,20 @@
 var main = function () {
     
+    //Brew Calculator
+    
+    $('#brewCalForm').submit(function() {
+        var coffeeValue = $('#coffeeDose').val();
+        var waterValue = $('#waterDose').val();
+        
+        function findRatio (coffee, water) {
+            return Math.round((parseFloat(water) / parseFloat(coffee)) * 10) / 10;
+        }
+        
+        $('.answerBrew').replaceWith('<p class="answerBrew">1 : ' + findRatio(coffeeValue, waterValue) + ' Ratio</p>');
+        
+        return false;
+    });
+    
     //Loss Calculator
     
     $('#lossCalForm').submit(function(){
@@ -13,29 +28,29 @@ var main = function () {
         var lossPercent = findLossPercent(preWeightValue, postWeightValue);
         $('.answerLoss').replaceWith('<p class="answerLoss">' + lossPercent + '%</p>');
         
-        
         return false;
     });
     
     
     //Dev Calculator
-    $('#devCalForm').submit(function() {
+    $('#devCalForm').submit(function(){
     
-        var totalMin = $('#totalMin').val();
-        var totalSec = $('#totalSec').val();
-        var devMin = $('#devMin').val();
-        var devSec = $('#devSec').val();
+        var totalMinValue = $('#totalMin').val();
+        var totalSecValue = $('#totalSec').val();
+        var devMinValue = $('#devMin').val();
+        var devSecValue = $('#devSec').val();
 
         function findSeconds (min, sec) {
-            return (min * 60) + sec;
+            return (parseFloat(min) * 60) + parseFloat(sec);
         }
 
-        var totalTime = findSeconds(totalMin, totalSec);
-        var totalDev = findSeconds(devMin, devSec);
-
+        var totalTime = findSeconds(totalMinValue, totalSecValue);
+        var totalDev = findSeconds(devMinValue, devSecValue);
+        
         function findPercent (total, dev){
-            return Math.round((100 - ((dev / total) * 100)) * 100) / 100;
+            return Math.round((100 - ((dev / total) * 100)) * 10) / 10;   
         }
+        
         var devPercent = findPercent(totalTime, totalDev);
         $('.answerDev').replaceWith('<p class="answerDev">' + devPercent + '%</p>');
         
